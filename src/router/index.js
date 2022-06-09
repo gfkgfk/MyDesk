d8ce55fc5627b7f533e4e790f36f810dca7c5646
gfkgfk <gfk000@126.com>
2022-06-09 17:56:56 +0800

    add digitalrain effect

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ export default new Router({
     routes: [
         {
             path: '/',
-            redirect: '/heart',
+            redirect: '/main',
         },
         {
             path: '/404',
