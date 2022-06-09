c816acb69acf9ef1d87a5fe6e589e131df6eee94
gfkgfk <gfk000@126.com>
2022-06-09 11:09:07 +0800

    format code
    format code

--- mydesk/src/views/login/loginpages/login.vue
+++ mydesk/src/views/login/loginpages/login.vue
@@ -233,30 +233,30 @@ export default {
 .red {
     color: red;
 }
-  .el-row {
+.el-row {
     margin-bottom: 20px;
     &:last-child {
-      margin-bottom: 0;
+        margin-bottom: 0;
     }
-  }
-  .el-col {
+}
+.el-col {
     border-radius: 4px;
-  }
-  .bg-purple-dark {
+}
+.bg-purple-dark {
     background: #99a9bf;
-  }
-  .bg-purple {
+}
+.bg-purple {
     background: #d3dce6;
-  }
-  .bg-purple-light {
+}
+.bg-purple-light {
     background: #e5e9f2;
-  }
-  .grid-content {
+}
+.grid-content {
     border-radius: 4px;
     min-height: 36px;
-  }
-  .row-bg {
+}
+.row-bg {
     padding: 10px 0;
     background-color: #f9fafc;
-  }
+}
 </style>
